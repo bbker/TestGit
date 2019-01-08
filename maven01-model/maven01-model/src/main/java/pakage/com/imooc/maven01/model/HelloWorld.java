6fb48157cf49bb2992c9d4d2778b2ca6775c465f
wh <q769880475@163.com>
2019-01-08 09:35:48 +0800

    更新

---
+++ maven01-model/maven01-model/src/main/java/pakage/com/imooc/maven01/model/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.imooc.maven01.model;
+import org.junit.*;
+import org.junit.Assert.*;
+public class HelloWorld {
+	public static void main(String[] args)
+{
+System.our.println(sayHello());
+}
+        public static String sayHello() {
+            return("hello");
+    }
+}
